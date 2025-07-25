30a303e47a87b4ea010f040580ef42995bb878bb
4meta5 <asinghchrony@protonmail.com>
2025-07-25 15:25:07 -0400

    more benchmarking changes required for registrar to not trigger ci

--- primitives/traits/src/lib.rs
+++ primitives/traits/src/lib.rs
@@ -455,7 +455,6 @@ pub trait RegistrarHandler<AccountId> {
     fn deregister(id: ParaId);
     fn deregister_weight() -> Weight;
 
-    #[cfg(feature = "runtime-benchmarks")]
     fn bench_head_data() -> Option<HeadData> {
         None
     }
@@ -463,7 +462,6 @@ pub trait RegistrarHandler<AccountId> {
     fn add_trusted_validation_code(_code: Vec<u8>) {}
     #[cfg(feature = "runtime-benchmarks")]
     fn registrar_new_session(_session: u32) {}
-    #[cfg(feature = "runtime-benchmarks")]
     fn prepare_chain_registration(_id: ParaId, _who: AccountId) {}
 }
 
